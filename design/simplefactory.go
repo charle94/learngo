d111bb8852ba386ec579204627ccf9b514392ff5
charle94 <826336279@qq.com>
2017-03-31 23:07:58 +0800

    factory pattern

--- design/simplefactory.go
+++ design/simplefactory.go
@@ -3,3 +3,43 @@ package design
 type Operation interface {
 	GetResult() (result float64)
 }
+type Add struct {
+	Data []float64
+}
+type Muti struct {
+	Data []float64
+}
+type Min struct {
+	Data []float64
+}
+type Divi struct {
+	Data []float64
+}
+
+func (m Muti) GetResult() (result float64) {
+	result = 1
+	for _, v := range m.Data {
+		result = result * v
+	}
+	return
+}
+func (a Add) GetResult() (result float64) {
+	for _, v := range a.Data {
+		result = result + v
+	}
+	return
+}
+func (m Min) GetResult() (result float64) {
+	result = m.Data[0]
+	for i := 1; i < len(m.Data); i++ {
+		result = result - m.Data[i]
+	}
+	return
+}
+func (d Divi) GetResult() (result float64) {
+	result = d.Data[0]
+	for i := 1; i < len(d.Data); i++ {
+		result = result / d.Data[i]
+	}
+	return
+}
---
+++ design/simplefactory_test.go
@@ -0,0 +1,22 @@
+package design
+
+import (
+	"testing"
+)
+
+func Test_operation(t *testing.T) {
+	var a []Operation
+	add := Add{Data: []float64{1, 4, 5, 7, 9, 12}}
+	a = append(a, add)
+	a = append(a, Min{Data: []float64{1, 3, 6, 9}})
+	a = append(a, Divi{Data: []float64{1, 3, 6, 9}})
+	for i := 0; i < len(a); i++ {
+		result := a[i].GetResult()
+		if i == 1 {
+			if -17 != result {
+				t.Error("error with getting result")
+			}
+		}
+	}
+
+}
